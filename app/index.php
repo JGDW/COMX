6d66676602c0cf3fee8847c24f3635f30adfa9fb
Stephen Munro <37016649+Stephen-Munro@users.noreply.github.com>
2018-04-17 23:57:45 +0200

    Changed index.php carousel with relevant images

--- app/index.php
+++ app/index.php
@@ -40,7 +40,7 @@
 		<div class="carousel-inner">
 		  <div class="item active">
 		  <div class="container">
-			<a href="register.html"><img style="width:100%" src="themes/images/carousel/1.png" alt="special offers"/></a>
+			<a href="register.html"><img style="width:100%" src="./assets/images/Specials/1.jpg" alt="special offers"/></a>
 			<div class="carousel-caption">
 				  <h4>Second Thumbnail label</h4>
 				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
@@ -49,7 +49,7 @@
 		  </div>
 		  <div class="item">
 		  <div class="container">
-			<a href="register.html"><img style="width:100%" src="themes/images/carousel/2.png" alt=""/></a>
+			<a href="register.html"><img style="width:100%" src="./assets/images/Specials/2.jpg" alt=""/></a>
 				<div class="carousel-caption">
 				  <h4>Second Thumbnail label</h4>
 				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
@@ -58,7 +58,7 @@
 		  </div>
 		  <div class="item">
 		  <div class="container">
-			<a href="register.html"><img src="themes/images/carousel/3.png" alt=""/></a>
+			<a href="register.html"><img src="./assets/images/Specials/3.jpg" width=100% height=100% alt=""/></a>
 			<div class="carousel-caption">
 				  <h4>Second Thumbnail label</h4>
 				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
@@ -66,9 +66,9 @@
 			
 		  </div>
 		  </div>
-		   <div class="item">
+		  <!--   <div class="item">
 		   <div class="container">
-			<a href="register.html"><img src="themes/images/carousel/4.png" alt=""/></a>
+			<a href="register.html"><img  src="./assets/images/Specials/4.jpg" width=100% height=100% alt=""/></a>
 			<div class="carousel-caption">
 				  <h4>Second Thumbnail label</h4>
 				  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
@@ -94,7 +94,9 @@
 				</div>
 		  </div>
 		  </div>
+
 		</div>
+		  -->
 		<a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
 		<a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
 	  </div> 
@@ -106,15 +108,15 @@
 	<div id="sidebar" class="span3">
 		<div class="well well-small"><a id="myCart" href="product_summary.html"><img src="themes/images/ico-cart.png" alt="cart">3 Items in your cart  <span class="badge badge-warning pull-right">$155.00</span></a></div>
 		<ul id="sideManu" class="nav nav-tabs nav-stacked">
-			<li class="subMenu open"><a> ELECTRONICS [230]</a>
+		<li class="subMenu open"><a> Components </a>
 				<ul>
-				<li><a class="active" href="products.php"><i class="icon-chevron-right"></i>Cameras (100) </a></li>
-				<li><a href="products.php"><i class="icon-chevron-right"></i>Computers, Tablets & laptop (30)</a></li>
-				<li><a href="products.php"><i class="icon-chevron-right"></i>Mobile Phone (80)</a></li>
-				<li><a href="products.php"><i class="icon-chevron-right"></i>Sound & Vision (15)</a></li>
+				<li><a class="active" href="products.php"><i class="icon-chevron-right"></i>CPUs </a></li>
+				<li><a href="products.php"><i class="icon-chevron-right"></i>Graphics Cards</a></li>
+				<li><a href="products.php"><i class="icon-chevron-right"></i>Mother Boards</a></li>
+				<li><a href="products.php"><i class="icon-chevron-right"></i>RAM</a></li>
 				</ul>
 			</li>
-			<li class="subMenu"><a> CLOTHES [840] </a>
+			<li class="subMenu"><a> Desktops </a>
 			<ul style="display:none">
 				<li><a href="products.php"><i class="icon-chevron-right"></i>Women's Clothing (45)</a></li>
 				<li><a href="products.php"><i class="icon-chevron-right"></i>Women's Shoes (8)</a></li>												
@@ -125,7 +127,7 @@
 				<li><a href="products.php"><i class="icon-chevron-right"></i>Kids Shoes (3)</a></li>												
 			</ul>
 			</li>
-			<li class="subMenu"><a>FOOD AND BEVERAGES [1000]</a>
+	   <!-- <li class="subMenu"><a>Graphics Cards</a>
 				<ul style="display:none">
 				<li><a href="products.php"><i class="icon-chevron-right"></i>Angoves  (35)</a></li>
 				<li><a href="products.php"><i class="icon-chevron-right"></i>Bouchard Aine & Fils (8)</a></li>												
@@ -136,10 +138,10 @@
 				<li><a href="products.php"><i class="icon-chevron-right"></i>Garden (3)</a></li>												
 				<li><a href="products.php"><i class="icon-chevron-right"></i>Khao Shong (11)</a></li>												
 			</ul>
-			</li>
-			<li><a href="products.php">HEALTH & BEAUTY [18]</a></li>
-			<li><a href="products.php">SPORTS & LEISURE [58]</a></li>
-			<li><a href="products.php">BOOKS & ENTERTAINMENTS [14]</a></li>
+			</li>-->
+			<li><a href="products.php">Laptops</a></li>
+			<!-- <li><a href="products.php">SPORTS & LEISURE [58]</a></li>
+			<li><a href="products.php">BOOKS & ENTERTAINMENTS [14]</a></li>-->
 		</ul>
 		<br/>
 		  <div class="thumbnail">
