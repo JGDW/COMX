97f98d001b20856e9cb74e8772f20f20d87c9adc
Johan de Waal <u16155140@tuks.co.za>
2018-04-17 22:19:37 +0200

    Updated navbar

--- app/navbar.php
+++ app/navbar.php
@@ -1,4 +1,78 @@
+<div id="welcomeLine" class=" row" style="background-color: #3A3F44; background-image: linear-gradient(#52575C, #3A3F44 70%, #3A3F44);">
+        <div class="pull-right">
+                   <a href="#login" role="button" data-toggle="modal" style="padding-right:0"><span class="btn btn-mini btn-primary">Login</span></a>
+
+                   <a href="#login" role="button" data-toggle="modal" style="padding-right:0"><span class="btn btn-mini btn-primary">Register</span></a>
+                   <div id="login" class="modal hide fade in" tabindex="-1" role="dialog" aria-labelledby="login" aria-hidden="false" >
+                       <div class="modal-header">
+                           <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                           <h3>Login Block</h3>
+                       </div>
+                       <div class="modal-body">
+                           <form class="form-horizontal loginFrm">
+                               <div class="control-group">
+                                   <input type="text" id="inputEmail" placeholder="Email">
+                               </div>
+                               <div class="control-group">
+                                   <input type="password" id="inputPassword" placeholder="Password">
+                               </div>
+                               <div class="control-group">
+                                   <label class="checkbox">
+                                       <input type="checkbox"> Remember me
+                                   </label>
+                               </div>
+                           </form>
+                           <button type="submit" class="btn btn-success">Sign in</button>
+                           <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+                       </div>
+                   </div>
+
+            <a href="product_summary.html"><span class="btn btn-mini btn-primary"><i class="icon-shopping-cart icon-white"></i>[ 3 ]</span> </a>
+        </div>
+</div>
 <div id="logoArea" class="navbar">
+
+    <a id="smallScreen" data-target="#topMenu" data-toggle="collapse" class="btn btn-navbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </a>
+
+
+    <div class="navbar-inner" style="border-radius: 0; background-color: #3A3F44;background-image: linear-gradient(#52575C, #3A3F44 70%, #3A3F44);">
+
+        <a class="brand" href="index.html"><img src="themes/images/logo.png" alt="Bootsshop"/></a>
+        <!--search bar-->
+        <form class="form-inline navbar-search" method="post" action="products.html"  style="margin-left: 2rem;">
+            <select class="srchTxt" style=" margin-right: -6px; width: auto;">
+                <option>All</option>
+                <option>CLOTHES </option>
+                <option>FOOD AND BEVERAGES </option>
+                <option>HEALTH & BEAUTY </option>
+                <option>SPORTS & LEISURE </option>
+                <option>BOOKS & ENTERTAINMENTS </option>
+            </select>
+            <input id="srchFld" class="srchTxt" type="text"  style="width: 420px;"/>
+            <button type="submit" id="submitButton" class="btn" style="margin-left: -5px; border-top-right-radius: 50px; border-bottom-right-radius: 50px; height: 28px;">Go</button>
+        </form>
+
+        <ul id="topMenu" class="nav pull-right" >
+            <li class=""><a href="special_offer.html">Home</a></li>
+            <li class=""><a href="normal.html">Departments</a></li>
+            <li class=""><a href="contact.html">Deals</a></li>
+            <li class=""><a href="contact.html">Contact us</a></li>
+
+
+        </ul>
+
+
+    </div>
+
+
+</div>
+
+
+<!-- <div id="logoArea" class="navbar">
 <a id="smallScreen" data-target="#topMenu" data-toggle="collapse" class="btn btn-navbar">
 	<span class="icon-bar"></span>
 	<span class="icon-bar"></span>
@@ -52,4 +126,4 @@
   </div>
 </div>
 </div>
-</div>
+</div> -->
