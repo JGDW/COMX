0d567d59ffbf8dfd6bac21fa8abae9a25123f276
Byron Antak <bma.byron.antak@gmail.com>
2018-04-21 21:49:15 +0200

    Compare page

--- app/compare.php
+++ app/compare.php
@@ -2,6 +2,21 @@
 <html lang="en">
   <head>
     <?php include ('header.php');?>
+    <style>
+    	.compare-img {
+    		max-height: 300px; 
+    		max-width: 300px;
+    	}
+    	.green-text {
+    		/* color: #59E817; */
+    	}
+    	.difference {
+    		color: #A617E8;
+    	}
+    	.feature {
+    		font-weight: bolder;
+    	}
+	</style>
   </head>
 <body>
 	<?php include ('navbar.php');?>
@@ -18,75 +33,86 @@
                     <li><a href="index.html">Home</a> <span class="divider">/</span></li>
                     <li class="active">Product Comparison</li>
                 </ul>
-                <h3> Product Comparison <small class="pull-right"> 2 products are compared </small></h3>
+                <a href="products.html" class="btn btn-large"><i class = "icon-chevron-left"></i> Back Products Page</a>
+                <h3> Product Comparison <small class="pull-right"> 2 products are being compared </small></h3>
                 <hr class="soft" />
-
                 <table id="compairTbl" class="table table-bordered">
                     <thead>
                         <tr>
-                            <th>Features</th>
-                            <th>Product1 name here </th>
-                            <th>Product2 name here</th>
+                            <th> Features </th>
+                            <th> Lenovo V310 </th>
+                            <th> Lenovo IdeaPad 320 </th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <td>&nbsp;</td>
-                            <td style="text-align:center">
+
+                            <td class = "td-limit-size" style="text-align:center">
+
+                                <img class = "compare-img" src="assets/images/laptops/lenovo-v310-core-i5-pro-laptop-deal-1000px-v1-0003.jpg" alt="" />
                                 <p class="justify">
-                                    Nowadays the lingerie industry is one of the most successful business spheres. We always stay in touch with the latest fashion tendencies - that is why our goods are so popular and we have a great number of faithful customers all over the country.
+                                    The Lenovo V310 is designed to meet all of your business needs, including budget. It's very configurable and has a range of options, from state-of-the-art processing to a flexible dual storage system. Plus, with its optional battery feature, this portable laptop can keep going for up to 12 hours
                                 </p>
-                                <img src="themes/images/products/1.jpg" alt="" />
-                                <form class="form-horizontal qtyFrm">
-                                    <h3> $222.00</h3>
-                                    <br/>
-                                    <a href="product_details.html" class="btn btn-large btn-primary"> Add to <i class=" icon-shopping-cart"></i></a>
-                                    <a href="product_details.html" class="btn btn-large"><i class="icon-zoom-in"></i></a>
-                                </form>
-                            </td>
-                            <td>
-                                <p class="justify">
-                                    Nowadays the lingerie industry is one of the most successful business spheres. We always stay in touch with the latest fashion tendencies - that is why our goods are so popular and we have a great number of faithful customers all over the country.
-                                </p>
-                                <img src="themes/images/products/3.jpg" alt="" />
 
                                 <form class="form-horizontal qtyFrm">
-                                    <h3> $190.00</h3>
+                                    <h3> R10,499.00</h3>
                                     <br/>
-                                    <a href="product_details.html" class="btn btn-large btn-primary"> Add to <i class=" icon-shopping-cart"></i></a>
-                                    <a href="product_details.html" class="btn btn-large"><i class="icon-zoom-in"></i></a>
+                                    <a class="btn btn-large btn-primary" href="#">Add to <i class="icon-shopping-cart"></i></a>
+                                </form>
+                            </td>
+                            <td class = "td-limit-size" style="text-align:center">
+                            	<img class = "compare-img" src="assets/images/laptops/lenovo-ideapad-320-core-i5-laptop-deal-1000px-v1-00071.jpg" alt="" />
+                                <p class="justify">
+                                    Everything about the IdeaPad 320 is designed to simplify your life. It will handle any task with ease, thanks to powerful processing. Preloaded with Windows 10 Home, you'll have the personal assistance of Cortana, designed to help open apps and answer your questions — whether typed or spoken. From the new streamlined design to a cleaner desktop interface, you'll enjoy the simplicity that IdeaPad 320 offers.
+                                </p>
+                                
+                                <form class="form-horizontal qtyFrm">
+                                    <h3 class = "green-text"> R9,199.00</h3>
+                                    <br/>
+                                    <a class="btn btn-large btn-primary" href="#">Add to <i class="icon-shopping-cart"></i></a>
                                 </form>
                             </td>
                         </tr>
-                        <tr>
-                            <td>Height</td>
-                            <td>2"</td>
-                            <td>2"</td>
-                        </tr>
-                        <tr>
-                            <td>Deepth</td>
-                            <td>5"</td>
-                            <td>5"</td>
-                        </tr>
-                        <tr>
-                            <td>Dimension</td>
-                            <td>--</td>
-                            <td>--</td>
-                        </tr>
-                        <tr>
-                            <td>Width</td>
-                            <td>6.5"</td>
-                            <td>6.5"</td>
-                        </tr>
-                        <tr>
-                            <td>Weight</td>
-                            <td>0.5kg</td>
-                            <td>0.5kg</td>
-                        </tr>
+                        <?php 
+                        	$fileContents = file_get_contents('./json/lenovo-laptops.json');
+                        	$json = json_decode($fileContents, true);
+                        	reset($json);
+                        	$firstKey = key($json);
+                        	$keys = array_keys($json[$firstKey]);
+
+                    		foreach($keys as $key) {
+                    			$str = '<tr>';
+                        		$str .= '<td class = "feature">' . $key . '</td>';
+								$arr = [];
+                        		foreach($json as $product) {
+                        			$val = $product[$key]['value'];
+                        			array_push($arr, $val);
+                        		}
+                        		$size = count($arr);
+                        		$isDifferent = false;
+                        		for($i = 0; $i < $size - 1; $i++) {
+                        			if ($arr[$i] != $arr[$i+1]) {
+                        				$isDifferent = true;
+                        				break;
+                        			}
+                        		}
+                        		if($isDifferent) {
+                        			for($i = 0; $i < $size; $i++) {
+	                        			$str .= '<td class = "difference">' . $arr[$i] . '</td>';
+	                        		}
+                        		}
+                        		else {
+                        			for($i = 0; $i < $size; $i++) {
+	                        			$str .= '<td>' . $arr[$i] . '</td>';
+	                        		}
+                        		}
+                        		$str .= '</tr>';
+                            	echo $str;
+                    		}
+                        ?>
                     </tbody>
                 </table>
-                <a href="products.html" class="btn btn-large pull-right">Back Products Page</a>
-
             </div>
         </div>
     </div>
--- app/index.php
+++ app/index.php
@@ -132,17 +132,12 @@
 		</div>
 	</div>
 </div>
-<!-- Footer ================================================================== -->
+	<!-- Footer ================================================================== -->
 	<?php include('footer.php'); ?>
-<!-- Placed at the end of the document so the pages load faster ============================================= -->
-	<script src="themes/js/jquery.js" type="text/javascript"></script>
-	<script src="themes/js/bootstrap.min.js" type="text/javascript"></script>
-	<script src="themes/js/google-code-prettify/prettify.js"></script>
-	
-	<script src="themes/js/bootshop.js"></script>
-    <script src="themes/js/jquery.lightbox-0.5.js"></script>
-	
+	<!-- Placed at the end of the document so the pages load faster ============================================= -->
+		
 	<!-- Themes switcher section ============================================================================================= -->
+
 <span id="themesBtn"></span>
 </body>
 </html>
