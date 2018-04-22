a79e4373d19137ce8761b83b4570df47c821f988
Tatenda <mafungatatenda@gmail.com>
2018-04-22 14:16:28 +0200

    For the product details. Fixed the html to php for redirection purposes

--- app/index.php
+++ app/index.php
@@ -74,55 +74,55 @@
 			  <ul class="thumbnails">
 				<li class="span3">
 				  <div class="thumbnail"style="max-height:289px; max-width:260px">
-					<a  href="product_details.html"><img src="./assets/images/frontpage/nzxt-h700i-black-red-mid-tower-gaming-case-300px-v1.jpg" alt="" style="max-width: 160px"/></a>
+					<a  href="product_details.php"><img src="./assets/images/frontpage/nzxt-h700i-black-red-mid-tower-gaming-case-300px-v1.jpg" alt="" style="max-width: 160px"/></a>
 					<div class="caption">
 					  <h5>NZXT H700i Black/Red</h5>
-					  <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">R 2,499</a></h4>
+					  <h4 style="text-align:center"><a class="btn" href="product_details.php"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">R 2,499</a></h4>
 					</div>
 				  </div>
 				</li>
 				<li class="span3">
 				  <div class="thumbnail"style="max-height:289px; max-width:260px">
-					<a  href="product_details.html"><img src="./assets/images/frontpage/razer-naga-epic-chroma-wireless-gaming-mouse-011.jpg" alt="" style="max-width: 160px"/></a>
+					<a  href="product_details.php"><img src="./assets/images/frontpage/razer-naga-epic-chroma-wireless-gaming-mouse-011.jpg" alt="" style="max-width: 160px"/></a>
 					<div class="caption">
 					  <h5>Raza Naga Epic Wireless</h5>
-					 <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">R 2,039</a></h4>
+					 <h4 style="text-align:center"><a class="btn" href="product_details.php"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">R 2,039</a></h4>
 					</div>
 				  </div>
 				</li>
 				<li class="span3">
 				  <div class="thumbnail"style="max-height:289px; max-width:260px">
-					<a  href="product_details.html"><img src="./assets/images/frontpage/corsair-h150i-pro-rgb-liquid-cpu-cooler-300px-v3.jpg" alt=""style="max-width: 160px; max-height:160px"/></a>
+					<a  href="product_details.php"><img src="./assets/images/frontpage/corsair-h150i-pro-rgb-liquid-cpu-cooler-300px-v3.jpg" alt=""style="max-width: 160px; max-height:160px"/></a>
 					<div class="caption">
 					  <h5>Corsair H150i Pro</h5>
-					   <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">R 2,349</a></h4>
+					   <h4 style="text-align:center"><a class="btn" href="product_details.php"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">R 2,349</a></h4>
 					</div>
 				  </div>
 				</li>
 				<li class="span3">
 				  <div class="thumbnail" style="max-height:289px; max-width:260px">
-					<a  href="product_details.html"><img src="./assets/images/frontpage/dell-p3418hw-34-inch-fhd-curved-monitor-300px-v1.jpg" alt=""style="max-width: 160px"/></a>
+					<a  href="product_details.php"><img src="./assets/images/frontpage/dell-p3418hw-34-inch-fhd-curved-monitor-300px-v1.jpg" alt=""style="max-width: 160px"/></a>
 					<div class="caption">
 					  <h5>Dell P3418HW 34inch</h5>
-					  <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">R 8,999</a></h4>
+					  <h4 style="text-align:center"><a class="btn" href="product_details.php"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">R 8,999</a></h4>
 					</div>
 				  </div>
 				</li>
 				<li class="span3">
 				  <div class="thumbnail"style="max-height:289px; max-width:260px">
-					<a  href="product_details.html"><img src="./assets/images/frontpage/vertagear-sl5000-black-white-300px-v1.jpg" alt=""style="max-width: 160px"/></a>
+					<a  href="product_details.php"><img src="./assets/images/frontpage/vertagear-sl5000-black-white-300px-v1.jpg" alt=""style="max-width: 160px"/></a>
 					<div class="caption">
 					  <h5>Vertagear SL5000 Black/White</h5>
-					  <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">R2199</a></h4>
+					  <h4 style="text-align:center"><a class="btn" href="product_details.php"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">R2199</a></h4>
 					</div>
 				  </div>
 				</li>
 				<li class="span3">
 				  <div class="thumbnail"style="max-height:289px; max-width:260px">
-					<a  href="product_details.html"><img src="./assets/images/frontpage/ASUS-PG348Q-ROG-Swift-G-Sync-Gaming-Monitor-300px-v2" alt=""style="max-width: 160px"/></a>
+					<a  href="product_details.php"><img src="./assets/images/frontpage/ASUS-PG348Q-ROG-Swift-G-Sync-Gaming-Monitor-300px-v2" alt=""style="max-width: 160px"/></a>
 					<div class="caption">
 					  <h5>ASUS PG348Q ROG Swift</h5>
-					   <h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">R 17,499</a></h4>
+					   <h4 style="text-align:center"><a class="btn" href="product_details.php"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">R 17,499</a></h4>
 					</div>
 				  </div>
 				</li>
